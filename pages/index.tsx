bbfc317210a26acefe73adec4d657f34fbeea904
arthurcezars <arthurcezars@gmail.com>
2022-10-11 23:26:34 -0300

    Adicionado serviço de consumo da api para listar os professores. Adicionado serviço para formatar valores para moeda no formato brasileiro e tamanho maximo da descrição.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,26 @@
-import { Box } from '@mui/material'
+import { Box, Dialog, Grid } from '@mui/material'
 import type { NextPage } from 'next'
-import { Professor } from '../src/@types/professor'
 import Lista from '../src/components/Lista/Lista'
+import { useIndex } from '../src/hooks/pages/useIndex'
 
 const Home: NextPage = () => {
-  const professores: Professor[] = [
-    {
-      id: 1,
-      nome: "Professor 1",
-      foto: "https://github.com/arthurcezars.png",
-      descricao: "Descrição do professor 1",
-      valor_hora: 100
-    },
-    {
-      id: 2,
-      nome: "Professor 2",
-      foto: "https://github.com/arthurcezars.png",
-      descricao: "Descrição do professor 2",
-      valor_hora: 200
-    },
-    {
-      id: 3,
-      nome: "Professor 3",
-      foto: "https://github.com/arthurcezars.png",
-      descricao: "Descrição do professor 3",
-      valor_hora: 300
-    },
-    {
-      id: 4,
-      nome: "Professor 4",
-      foto: "https://github.com/arthurcezars.png",
-      descricao: "Descrição do professor 4",
-      valor_hora: 400
-    }
-  ]
+  const { listaProfessores } = useIndex();
 
   return (
     <>
-      <Box sx={{ backgroundColor: 'secondary.main' }}>
-        <Lista professores={professores}></Lista>
-      </Box>
+      <div>
+        <Box sx={{ backgroundColor: 'secondary.main' }}>
+          <Lista professores={listaProfessores}></Lista>
+        </Box>
+
+        <Dialog open={true} fullWidth PaperProps={{sx: {p: 5}}}>
+          <Grid container>
+            <Grid item>
+              teste
+            </Grid>
+          </Grid>
+        </Dialog>
+      </div>
     </>
   )
 }
--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@mui/material";
 import { Professor } from "../../@types/professor";
+import { FormatadorService } from "../../services/FormatadorService";
 import { Foto, Informacoes, Nome, ItemLista, ListaStyled, Valor, Descricao, ListaVazia } from "./Lista.style";
 
 interface ListaProps {
@@ -16,8 +17,8 @@ const Lista = (props: ListaProps) => {
                         <Foto src={professor.foto}></Foto>
                         <Informacoes>
                             <Nome>{professor.nome}</Nome>
-                            <Valor>{professor.valor_hora.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })} por hora</Valor>
-                            <Descricao>{professor.descricao}</Descricao>
+                            <Valor>{FormatadorService.valorMonetario(professor.valor_hora)} por hora</Valor>
+                            <Descricao>{FormatadorService.limitarTexto(professor.descricao, 200)}</Descricao>
                             <Button sx={{ width: '70%' }}>Marcar Aula com {professor.nome}</Button>
                         </Informacoes>
                     </ItemLista>
